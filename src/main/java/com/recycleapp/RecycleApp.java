8889b35b9791a461a6e04c68fd344373eef0f9d6
Nallekari <tuomas.nallikari@outlook.com>
2023-02-13 13:32:12 +0200

    added repositories

--- src/main/java/com/recycleapp/RecycleApp.java
+++ src/main/java/com/recycleapp/RecycleApp.java
@@ -2,6 +2,7 @@ package com.recycleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.*;
 import java.util.*;
@@ -13,4 +14,7 @@ public class RecycleApp {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RecycleApp.class, args);
     }
+
+
+    @Bean
 }
