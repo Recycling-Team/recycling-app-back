2c57d4602dbc94814a0a88f120a409e58361f968
troksii <santeri.pulkka@gmail.com>
2023-02-13 13:30:34 +0200

    user changes

--- src/main/java/com/recycleapp/model/User.java
+++ src/main/java/com/recycleapp/model/User.java
@@ -1,10 +1,27 @@
 package com.recycleapp.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+
+
 
 public class User {
+    @Id
+    @OneToMany
+    @JsonIgnoreProperties("")
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
 
     private String name;
 
-    public User() {
+    public User(Reservation reservation) {
+        super();
+		this.reservation = reservation;
     }
 
     public User(String name) {
@@ -18,4 +35,40 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    public User() {
+    }
+
+    public User(Reservation reservation, String name) {
+        this.reservation = reservation;
+        this.name = name;
+    }
+
+    public Reservation getReservation() {
+        return this.reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public User reservation(Reservation reservation) {
+        setReservation(reservation);
+        return this;
+    }
+
+    public User name(String name) {
+        setName(name);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " reservation='" + getReservation() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+
+  
 }
