d497e7441c29801c7a7f752c331aac74fc286861
Nallekari <tuomas.nallikari@outlook.com>
2023-02-13 11:06:03 +0200

    finally fixed capitalizatio to lower?

---
+++ src/main/java/com/recycleapp/RecycleApp.java
@@ -0,0 +1,12 @@
+package com.recycleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RecycleApp.class, args);
+    }
+}
