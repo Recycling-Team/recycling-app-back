7ef115e9f6ef989b2c26fe0b3dec47744c975128
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2023-02-13 13:37:55 +0200

    Item olio

--- src/main/java/com/recycleapp/model/Item.java
+++ src/main/java/com/recycleapp/model/Item.java
@@ -1,6 +1,20 @@
 package com.recycleapp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Item {
+
+    @Id
+    @OneToOne
+    @JsonIgnoreProperties("")
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
 
     private String name;
 
@@ -18,4 +32,21 @@ public class Item {
     public void setItem(String name) {
         this.name = name;
     }
+
+    private String getName() {
+        return null;
+    }
+
+    private String getReservation() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " reservation='" + getReservation() + "'" +
+                ", name='" + getName() + "'" +
+                "}";
+    }
+
 }
