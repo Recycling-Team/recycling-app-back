6a739e0aef9bb8658e32284ba2d60e82f03739e0
Nallekari <tuomas.nallikari@outlook.com>
2023-02-14 12:12:39 +0200

    added crossorigin to usercontroller

--- src/main/java/com/recycleapp/controller/UserController.java
+++ src/main/java/com/recycleapp/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.recycleapp.model.ReservationRepository;
 import com.recycleapp.model.UserRepository;
 import com.recycleapp.model.Usera;
 
+@CrossOrigin
 @Controller
 public class UserController {
 
