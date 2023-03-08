92cc437eb74f4b7751b503a1d1423b739c6eb4f5
troksii <santeri.pulkka@gmail.com>
2023-03-08 22:07:37 +0200

    changes to item and user

--- src/main/java/com/function/Item.java
+++ src/main/java/com/function/Item.java
@@ -1,14 +1,34 @@
 package com.function;
 
 public class Item {
-    public int item_id;
-    public String item_name;
+    private int item_id;
+    private String item_name;
+    private User user;
 
-    public Item() {
-    }
-
-    public Item(int item_id, String item_name) {
+    public Item(int item_id, String item_name, User user) {
         this.item_id = item_id;
         this.item_name = item_name;
+        this.user = user;
+        user.addItem(this);
+    }
+
+    public int getItemId() {
+        return item_id;
+    }
+
+    public String getItemName() {
+        return item_name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setItemName(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/com/function/User.java
+++ src/main/java/com/function/User.java
@@ -1,10 +1,14 @@
 package com.function;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String username;
     private String email;
+    private List<Item> items = new ArrayList<>();
 
-    public User(String username, String password, String email) {
+    public User(String username, String email) {
         this.username = username;
         this.email = email;
     }
@@ -23,5 +27,17 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
     }
 }
