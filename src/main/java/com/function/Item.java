b083e63b3be9bfb973370d6217ea2ce64bfbbe62
troksii <santeri.pulkka@gmail.com>
2023-03-09 17:52:35 +0200

    user and item class connections

--- src/main/java/com/function/Item.java
+++ src/main/java/com/function/Item.java
@@ -1,31 +1,33 @@
 package com.function;
 
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
 public class Item {
-    private int item_id;
-    private String item_name;
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
+    private int itemId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
-    public Item(int item_id, String item_name, User user) {
-        this.item_id = item_id;
-        this.item_name = item_name;
+    public Item() {
+    }
+
+    public Item(String itemName, User user) {
         this.user = user;
-        user.addItem(this);
     }
 
     public int getItemId() {
-        return item_id;
-    }
-
-    public String getItemName() {
-        return item_name;
+        return itemId;
     }
 
     public User getUser() {
         return user;
-    }
-
-    public void setItemName(String item_name) {
-        this.item_name = item_name;
     }
 
     public void setUser(User user) {
--- src/main/java/com/function/User.java
+++ src/main/java/com/function/User.java
@@ -2,15 +2,29 @@ package com.function;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.*;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int userId;
+    
     private String username;
     private String email;
+
+    @JoinColumn(name = "item_id")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Item> items = new ArrayList<>();
 
     public User(String username, String email) {
         this.username = username;
         this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public String getUsername() {
@@ -31,10 +45,12 @@ public class User {
 
     public void addItem(Item item) {
         items.add(item);
+        item.setUser(this);
     }
 
     public void removeItem(Item item) {
         items.remove(item);
+        item.setUser(null);
     }
 
     public List<Item> getItems() {
