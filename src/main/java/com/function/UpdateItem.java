71a9531103be4ce13a6cbfe0e740c14170894793
troksii <santeri.pulkka@gmail.com>
2023-04-26 10:58:02 +0300

    available to false

---
+++ src/main/java/com/function/UpdateItem.java
@@ -0,0 +1,45 @@
+package com.function;
+
+import com.common.Item;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.sql.annotation.SQLInput;
+import com.microsoft.azure.functions.sql.annotation.SQLOutput;
+import com.microsoft.azure.functions.ExecutionContext;
+
+
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class UpdateItem {
+    /**
+     * This function listens at endpoint "/api/HttpTriggerDatabase". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpTriggerDatabase
+     * 2. curl {your host}/api/HttpTriggerDatabase?name=HTTP%20Query
+     */
+    @FunctionName("UpdateItem")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {
+            HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS, route = "add-item") HttpRequestMessage<Optional<String>> request,
+            @SQLOutput(name = "item", commandText = "items", connectionStringSetting = "SqlConnectionString") OutputBinding<Item> item)
+            throws JsonParseException, JsonMappingException, IOException {
+        
+        // Update the availability of the item
+        Item item2 = new Item();
+        item2.setAvailable("false");
+        
+        // Return the updated item
+        return request.createResponseBuilder(HttpStatus.OK).header("Content-Type", "application/json").body(item).build();
+    }
+}
