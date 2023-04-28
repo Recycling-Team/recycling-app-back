955043ca79a971911b4e293ef8f5dd44478d62d4
troksii <santeri.pulkka@gmail.com>
2023-04-28 09:27:59 +0300

    deleteitem changes

--- src/main/java/com/function/DeleteItem.java
+++ src/main/java/com/function/DeleteItem.java
@@ -1,38 +1,38 @@
 package com.function;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Optional;
-
 import com.common.Item;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
+import com.microsoft.azure.functions.sql.annotation.SQLInput;
 import com.microsoft.azure.functions.sql.annotation.SQLOutput;
-import com.microsoft.azure.functions.ExecutionContext;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.List;
 
 public class DeleteItem {
-
     @FunctionName("DeleteItem")
     public void run(
-        // Timer which executes this function every day at 0:00 midnight
-        @TimerTrigger(name = "timer", schedule = "0 0 0 * * *") String timerInfo,
-
+        @TimerTrigger(name = "timerInfo", schedule = "0 0 0 * * *") String timerInfo,
+        @SQLInput(
+            name = "items",
+            commandText =  "SELECT * FROM dbo.items WHERE available = 'True'",
+            connectionStringSetting = "SqlConnectionString") List<Item> items,
         @SQLOutput(
             name = "item",
-            commandText = "UPDATE dbo.items SET available = 'false' WHERE listing_date < DATEADD(week, -2, GETDATE())",
-            connectionStringSetting = "SqlConnectionString") OutputBinding<Void> output,
+            commandText = "dbo.items",
+            connectionStringSetting = "SqlConnectionString") OutputBinding <Item> Item,
+        final ExecutionContext context) {
 
-        final ExecutionContext context)
-        throws JsonParseException, JsonMappingException, IOException {
-
-
-        }
+            for (Item item : items) {
+                LocalDate twoWeeksAgo = LocalDate.now(ZoneOffset.UTC).minusWeeks(2);
+                LocalDate itemListingDate = LocalDate.parse(item.getListing_date());
+                
+                if (itemListingDate.isBefore(twoWeeksAgo)) {
+                    item.setAvailable("False");
+                }
+            }
+    }
 }
